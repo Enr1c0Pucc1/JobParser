f04d1f11983ccdf92a3bfe8e2c4e0e8e8e13c484
Pucci <greenymanxcreative@gmail.com>
2023-11-10 18:09:10 +0300

    Оптимизация кода

--- hh_vacancies_collector.py
+++ hh_vacancies_collector.py
@@ -1,54 +1,55 @@
 import requests
-from main_functions import predict_rub_salary_hh, draw_table
+from help_functions import predict_rub_salary_hh
 
 
-def get_pages_for_lang(lang, area):
+def get_pages_for_lang(lang, area, per_page, period):
     params = {
         'text': lang,
         'area': area,
-        'per_page': 50,
-        'period': 30
+        'per_page': per_page,
+        'period': period
     }
     response = requests.get('https://api.hh.ru/vacancies', params=params)
     response.raise_for_status()
     return response.json()['pages']
 
 
-def get_vacancies_from_hh(lang, area, page):
+def get_vacancies_from_hh(lang, area, page, per_page, period):
     params = {
         'text': lang,
         'area': area,
         'page': page,
-        'per_page': 50,
-        'period': 30
+        'per_page': per_page,
+        'period': period
     }
     response = requests.get('https://api.hh.ru/vacancies', params=params)
     response.raise_for_status()
     return response.json()['items'], response.json()['found']
 
 
-def get_average_salary(programming_languages):
+def average_salary_hh(programming_languages):
+    vacancies_per_page = 50
+    period = 30  # days
     salaries_by_lang = {}
-    for i, plang in enumerate(programming_languages, start=0):
-        pages_number = get_pages_for_lang(programming_languages[i], 1)
-        vacancies_processed = 0
-        salaries = 0
+    for plang in programming_languages:
+        pages_number = get_pages_for_lang(plang, 1, vacancies_per_page, period)
+        vacancies_prcssd = 0
+        vacancies_with_salaries = 0
         for page in range(pages_number):
             vacancies, vacancies_found = get_vacancies_from_hh(
-                programming_languages[i], 1, page)
-            for m, vacancy in enumerate(vacancies, start=1):
+                plang, 1, page, vacancies_per_page, period)
+            for vacancy in vacancies:
                 salary = predict_rub_salary_hh(vacancy['salary'])
                 if salary:
-                    vacancies_processed += 1
-                    salaries += salary
+                    vacancies_prcssd += 1
+                    vacancies_with_salaries += salary
+                if vacancies_prcssd != 0:
+                    average_salary = vacancies_with_salaries//vacancies_prcssd
+                else:
+                    average_salary = 'N/A'
             salaries_by_lang[plang] = {
                     'vacancies_found': vacancies_found,
-                    'vacancies_processed': vacancies_processed,
-                    'average_salary': (salaries//vacancies_processed)
+                    'vacancies_processed': vacancies_prcssd,
+                    'average_salary': average_salary
                     }
     return salaries_by_lang
-
-
-def draw_hh_statistic(languages):
-    hh_statistic = get_average_salary(languages)
-    return draw_table(hh_statistic, "HeadHunter Moscow")
