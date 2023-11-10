809f47ee6e489b381dede2e9f35107bcffb21c0f
Pucci <greenymanxcreative@gmail.com>
2023-11-10 18:09:35 +0300

    Убраны ненужные функции

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from hh_vacancies_collector import draw_hh_statistic
-from sj_vacancies_collector import draw_superjob_statistic
+# from hh_vacancies_collector import average_salary_hh
+from sj_vacancies_collector import average_salary_sj
+from help_functions import draw_table
+from dotenv import load_dotenv
+import os
 
 
 if __name__ == '__main__':
@@ -12,5 +15,9 @@ if __name__ == '__main__':
         'C++',
         'C#',
         'Go']
-    print(draw_hh_statistic(programming_languages))
-    print(draw_superjob_statistic(programming_languages))
+    load_dotenv()
+    superjob_key = os.environ["SJ_API_KEY"]
+    # hh_statistic = average_salary_hh(programming_languages)
+    # draw_table(hh_statistic, "HeadHunter Moscow")
+    sj_statistic = average_salary_sj(programming_languages, superjob_key)
+    print(draw_table(sj_statistic, "SuperJob Moscow"))
