c399c8631808c185f72c51836be5797b03f012af
Pucci <greenymanxcreative@gmail.com>
2023-11-24 18:03:59 +0300

    Оптимизация
    Читать код стало проще

--- sj_vacancies_collector.py
+++ sj_vacancies_collector.py
@@ -1,54 +1,50 @@
 import requests
-from help_functions import predict_rub_salary_sj
+from help_functions import predict_rub_salary_sj, count_prcssd_vacancies
 
 
-def get_vacancies_from_sj(lang, page, api_key):
+def get_vacancies_from_sj(area, lang, catalogues, per_page, page, api_key):
     headers = {"X-Api-App-Id": api_key}
-    params = {'town': 4,
+    params = {'town': area,
               'keyword': lang,
-              'catalogues': 48,
-              'count': 100,
+              'catalogues': catalogues,
+              'count': per_page,
               'page': page}
     response = requests.get('https://api.superjob.ru/2.0/vacancies',
                             headers=headers,
                             params=params)
     response.raise_for_status()
     vacancies = response.json()
-    return vacancies['objects'], vacancies['total']
+    return vacancies['objects'], vacancies['more']
 
 
-def average_salary_sj(programming_languages, api_key):
+def get_average_salary_sj(programming_languages, api_key):
     salaries_by_lang = {}
-    vacancies_number_limit = 500
-    for plang in programming_languages:
-        pages = 1
+    area = 4  # Moscow ID (in SJ)
+    catalogue = 48
+    per_page = 100
+    for programming_language in programming_languages:
+        pages_left = True
         page = 0
         vacancies_found = 0
         vacancies_prcssd = 0
-        vacancies_with_salaries = 0
-        while page < pages:
-            vacancies, total = get_vacancies_from_sj(plang,
-                                                     page, api_key)
-            if total > vacancies_number_limit:
-                pages = vacancies_number_limit // 500
-            elif total == 0:
-                break
-            elif total < 100:
-                pages = 1
-            else:
-                pages = total // 100
+        vacancies_with_salary = 0
+        while pages_left:
+            vacancies, pages_left = get_vacancies_from_sj(area,
+                                                          programming_language,
+                                                          catalogue,
+                                                          per_page,
+                                                          page,
+                                                          api_key)
             page += 1
             for vacancy in vacancies:
                 salary = predict_rub_salary_sj(vacancy)
                 vacancies_found += 1
                 if salary:
                     vacancies_prcssd += 1
-                    vacancies_with_salaries += salary
-                if vacancies_prcssd != 0:
-                    average_salary = vacancies_with_salaries//vacancies_prcssd
-                else:
-                    average_salary = 'N/A'
-            salaries_by_lang[plang] = {
+                    vacancies_with_salary += salary
+                average_salary = count_prcssd_vacancies(vacancies_prcssd,
+                                                        vacancies_with_salary)
+            salaries_by_lang[programming_language] = {
                 'vacancies_found': vacancies_found,
                 'vacancies_processed': vacancies_prcssd,
                 'average_salary': average_salary
