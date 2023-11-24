f89696d3674bccc23e4d681d77be77b688c2aca6
Pucci <greenymanxcreative@gmail.com>
2023-11-24 18:03:47 +0300

    Добавлена таблица HH
    Убрал комментарии, которые случайно оставил при тестах

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# from hh_vacancies_collector import average_salary_hh
-from sj_vacancies_collector import average_salary_sj
+from hh_vacancies_collector import get_average_salary_hh
+from sj_vacancies_collector import get_average_salary_sj
 from help_functions import draw_table
 from dotenv import load_dotenv
 import os
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         'Go']
     load_dotenv()
     superjob_key = os.environ["SJ_API_KEY"]
-    # hh_statistic = average_salary_hh(programming_languages)
-    # draw_table(hh_statistic, "HeadHunter Moscow")
-    sj_statistic = average_salary_sj(programming_languages, superjob_key)
+    hh_statistic = get_average_salary_hh(programming_languages)
+    print(draw_table(hh_statistic, "HeadHunter Moscow"))
+    sj_statistic = get_average_salary_sj(programming_languages, superjob_key)
     print(draw_table(sj_statistic, "SuperJob Moscow"))
