e1869fd9387b6d72ff8eeb5108ebc6ced2832052
Pucci <greenymanxcreative@gmail.com>
2023-11-24 18:03:16 +0300

    Перенос функций и оптимизация
    Читать код стало проще

--- hh_vacancies_collector.py
+++ hh_vacancies_collector.py
@@ -1,17 +1,6 @@
 import requests
 from help_functions import predict_rub_salary_hh
-
-
-def get_pages_for_lang(lang, area, per_page, period):
-    params = {
-        'text': lang,
-        'area': area,
-        'per_page': per_page,
-        'period': period
-    }
-    response = requests.get('https://api.hh.ru/vacancies', params=params)
-    response.raise_for_status()
-    return response.json()['pages']
+from help_functions import count_prcssd_vacancies, get_pages_for_hh
 
 
 def get_vacancies_from_hh(lang, area, page, per_page, period):
@@ -24,30 +13,33 @@ def get_vacancies_from_hh(lang, area, page, per_page, period):
     }
     response = requests.get('https://api.hh.ru/vacancies', params=params)
     response.raise_for_status()
-    return response.json()['items'], response.json()['found']
+    vacancies = response.json()
+    return vacancies['items'], vacancies['found']
 
 
-def average_salary_hh(programming_languages):
+def get_average_salary_hh(programming_languages):
     vacancies_per_page = 50
     period = 30  # days
+    area = 1  # Moscow id
     salaries_by_lang = {}
-    for plang in programming_languages:
-        pages_number = get_pages_for_lang(plang, 1, vacancies_per_page, period)
+    for programming_language in programming_languages:
+        pages_number = get_pages_for_hh(programming_language,
+                                        area,
+                                        vacancies_per_page,
+                                        period)
         vacancies_prcssd = 0
-        vacancies_with_salaries = 0
+        vacancies_with_salary = 0
         for page in range(pages_number):
             vacancies, vacancies_found = get_vacancies_from_hh(
-                plang, 1, page, vacancies_per_page, period)
+                programming_language, area, page, vacancies_per_page, period)
             for vacancy in vacancies:
                 salary = predict_rub_salary_hh(vacancy['salary'])
                 if salary:
                     vacancies_prcssd += 1
-                    vacancies_with_salaries += salary
-                if vacancies_prcssd != 0:
-                    average_salary = vacancies_with_salaries//vacancies_prcssd
-                else:
-                    average_salary = 'N/A'
-            salaries_by_lang[plang] = {
+                    vacancies_with_salary += salary
+                average_salary = count_prcssd_vacancies(vacancies_prcssd,
+                                                        vacancies_with_salary)
+            salaries_by_lang[programming_language] = {
                     'vacancies_found': vacancies_found,
                     'vacancies_processed': vacancies_prcssd,
                     'average_salary': average_salary
