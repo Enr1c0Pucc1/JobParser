f54d33379dcacc87722f30081d8862f3af19dd8e
Pucci <greenymanxcreative@gmail.com>
2023-11-29 18:55:54 +0300

    Изменены названия чтобы было понятнее

--- help_functions.py
+++ help_functions.py
@@ -33,9 +33,9 @@ def predict_rub_salary_sj(vacancy):
     return average
 
 
-def count_processed_vacancies(vacancies_processed, vacancies_with_salaries):
-    if vacancies_processed:
-        average_salary = vacancies_with_salaries//vacancies_processed
+def count_prcssd_vacancies(vacancies_prcssd, vacancies_with_salaries):
+    if vacancies_prcssd:
+        average_salary = vacancies_with_salaries//vacancies_prcssd
     else:
         average_salary = 'N/A'
     return average_salary
